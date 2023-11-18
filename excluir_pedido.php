6a5b39d653535a31bf30ee2f2d5ab977ba14dbd8
PedroGois <pedromorais.gois@gmail.com>
2023-11-18 18:24:25 -0300

    Excluindo pedido junto com itents

--- excluir_pedido.php
+++ excluir_pedido.php
@@ -1,13 +1,14 @@
 <?php
 include('conexao.php');
 
-if (isset($_GET['pedido_id']) && is_numeric($_GET['pedido_id'])) {
-    $pedido_id = $_GET['pedido_id'];
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $pedido_id = $_GET['id'];
 
     // Excluir o pedido e produtos associados
     $sql_delete_itens = "DELETE FROM itens_pedido WHERE id_pedido = $pedido_id";
 
     if (mysqli_query($con, $sql_delete_itens)) {
+        echo "Produtos associados excluídos com sucesso.";
         $sql_delete_pedido = "DELETE FROM pedidos WHERE id = $pedido_id";
 
         if (mysqli_query($con, $sql_delete_pedido)) {
@@ -18,25 +19,8 @@ if (isset($_GET['pedido_id']) && is_numeric($_GET['pedido_id'])) {
     } else {
         echo "Erro ao excluir os produtos associados ao pedido: " . mysqli_error($con);
     }
-} elseif (isset($_GET['produto_id']) && is_numeric($_GET['produto_id'])) {
-    $produto_id = $_GET['produto_id'];
-
-    // Excluir o produto e seu pedido associado
-    $sql_delete_pedido_produto = "DELETE FROM itens_pedido WHERE id_produto = $produto_id";
-
-    if (mysqli_query($con, $sql_delete_pedido_produto)) {
-        $sql_delete_produto = "DELETE FROM produtos WHERE id = $produto_id";
-
-        if (mysqli_query($con, $sql_delete_produto)) {
-            echo "Produto e pedido associado excluídos com sucesso.";
-        } else {
-            echo "Erro ao excluir o produto: " . mysqli_error($con);
-        }
-    } else {
-        echo "Erro ao excluir o pedido associado ao produto: " . mysqli_error($con);
-    }
 } else {
-    echo "ID do pedido ou produto inválido";
+    echo "ID do pedido inválido";
 }
 
 mysqli_close($con);
